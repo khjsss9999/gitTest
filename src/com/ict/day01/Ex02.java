d2f787edab64cbcd7fddb5007b1da0cb0f3af835
khjsss999 <khjsss999@naver.com>
2024-02-07 10:55:46 +0900

    school-2

--- src/com/ict/day01/Ex02.java
+++ src/com/ict/day01/Ex02.java
@@ -3,5 +3,8 @@ package com.ict.day01;
 public class Ex02 {
 	public static void main(String[] args) {
 		System.out.println("깃 테스트");
+		System.out.println("깃 테스트2");
+		System.out.println("깃 테스트2");
+		
 	}
 }
