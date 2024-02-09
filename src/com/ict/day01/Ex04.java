7e4f72c319a9eb779b35fb169c8037385d7a7473
khjsss999 <khjsss999@naver.com>
2024-02-09 17:22:03 +0900

    homeict

--- src/com/ict/day01/Ex04.java
+++ src/com/ict/day01/Ex04.java
@@ -3,5 +3,6 @@ package com.ict.day01;
 public class Ex04 {
 	public static void main(String[] args) {
 		System.out.println("잘 되는구만");
+		System.out.println("목에 담 걸렸다");
 	}
 }
---
+++ src/com/ict/day01/Ex05.java
@@ -0,0 +1,7 @@
+package com.ict.day01;
+
+public class Ex05 {
+	public static void main(String[] args) {
+		System.out.println("잘 되고 있는거냐");
+	}
+}
