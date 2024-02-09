e1b8141988de0b8483e6ba8289b3ed5fc188d2e6
khjsss999 <khjsss999@naver.com>
2024-02-09 17:11:54 +0900

    home

--- src/com/ict/day01/Ex01.java
+++ src/com/ict/day01/Ex01.java
@@ -4,5 +4,6 @@ public class Ex01 {
 	public static void main(String[] args) {
 		System.out.println("hello java");
 		System.out.println("hello git");
+		System.out.println("오오오오");
 	}
 }
---
+++ src/com/ict/day01/Ex04.java
@@ -0,0 +1,7 @@
+package com.ict.day01;
+
+public class Ex04 {
+	public static void main(String[] args) {
+		System.out.println("잘 되는구만");
+	}
+}
